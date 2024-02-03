5e6ff449e99dd2caa8722291a8572604cbaf9b0d
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-02-03 17:44:13 +0200

    created entity class Client

---
+++ src/main/java/org/example/entity/Client.java
@@ -0,0 +1,21 @@
+package org.example.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "client")
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name")
+    private String name;
+}
